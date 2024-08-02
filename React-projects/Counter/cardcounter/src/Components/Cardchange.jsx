a20de20ce4c853785a671c9b866a404899cc6581
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2024-08-02 18:34:31 +0530

    react

--- React-projects/Counter/cardcounter/src/Components/Cardchange.jsx
+++ React-projects/Counter/cardcounter/src/Components/Cardchange.jsx
@@ -6,19 +6,19 @@ function Cardchange() {
     let [count, countState] = useState(0)
 
     function prev() {
-        if(count!=0){
+        if (count != 0) {
             countState(count - 1)
         }
     }
     function next() {
-        if(count < ProductData.length-1){
+        if (count < ProductData.length - 1) {
             countState(count + 1)
         }
     }
 
     const carddata = ProductData.map((products) => (
         <li key={products.id} className='list-unstyled'>
-            <div className="card" style={{ width: "260px", height: "500px" }}>
+            <div className="card border-2 border-success" style={{ width: "270px", height: "500px" }}>
                 <img src={products.image} className="card-img-top h-50 p-3 object-fit-contain" alt={products.title} />
                 <div className="card-body  d-flex flex-column justify-content-between">
                     <h5 className="card-title">{products.title.slice(0, 20)}</h5>
@@ -31,13 +31,15 @@ function Cardchange() {
     ))
 
     return (
-        <div className='d-flex justify-content-center'>
-            <button type="button" className="btn btn-success" onClick={prev}>prev</button>
-            <div >
-                {carddata[count]}
+        <>
+            <div className='d-flex justify-content-center align-items-center' style={{height : "100vh"}}>
+                <button type="button" className="btn btn-success h-25" onClick={prev}><i class="fa-solid fa-circle-left fa-lg" style={{color: "#ffffff"}}></i></button>
+                <div className='mx-5'>
+                    {carddata[count]}
+                </div>
+                <button type="button" className="btn btn-success h-25" onClick={next}><i class="fa-solid fa-circle-right fa-lg" style={{color: "#ffffff"}}></i></button>
             </div>
-            <button type="button" className="btn btn-success" onClick={next}>Next</button>
-        </div>
+        </>
     )
 }
 
