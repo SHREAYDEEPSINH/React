fdadfbd45b6e1cfd2c24cf707b62dc6278bf1069
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2025-02-27 22:57:19 +0530

    react

--- React-projects/e-com-web/src/firebase.jsx
+++ React-projects/e-com-web/src/firebase.jsx
@@ -17,20 +17,40 @@
 
 
 // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAWfoHw1ibg6EsAy8nwgwBn6Z2kjSYWx9Q",
+//   authDomain: "painter-20eca.firebaseapp.com",
+//   projectId: "painter-20eca",
+//   storageBucket: "painter-20eca.firebasestorage.app",
+//   messagingSenderId: "135398875666",
+//   appId: "1:135398875666:web:30e78733198f57bc6eeecb",
+//   databaseURL: "https://painter-20eca-default-rtdb.firebaseio.com"
+
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAWfoHw1ibg6EsAy8nwgwBn6Z2kjSYWx9Q",
-  authDomain: "painter-20eca.firebaseapp.com",
-  projectId: "painter-20eca",
-  storageBucket: "painter-20eca.firebasestorage.app",
-  messagingSenderId: "135398875666",
-  appId: "1:135398875666:web:30e78733198f57bc6eeecb",
-  databaseURL: "https://painter-20eca-default-rtdb.firebaseio.com"
-
+  apiKey: "AIzaSyCSaynupO5eixBoM_MaXw82n1x4BqEiMFk",
+  authDomain: "painter-63c17.firebaseapp.com",
+  projectId: "painter-63c17",
+  storageBucket: "painter-63c17.firebasestorage.app",
+  messagingSenderId: "699944108860",
+  appId: "1:699944108860:web:25573b829a72da0b265439",
+  databaseURL : "https://painter-63c17-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
